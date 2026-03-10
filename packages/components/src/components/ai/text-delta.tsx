41bd90b784c07e4df8ba6915eb0871a8c3d43996
MP281X <paludgnachmatteo.dev@gmail.com>
2026-03-10 08:16:38 +0100

    dev: cleanup

--- packages/components/src/components/ai/text-delta.tsx
+++
@@ -1,7 +0,0 @@
-import type {TextPart} from '@ai-toolkit/ai/schema'
-
-import {Markdown} from '#components/render/markdown.tsx'
-
-export function TextDelta(props: {part: TextPart}) {
-	return <Markdown>{props.part.text}</Markdown>
-}
